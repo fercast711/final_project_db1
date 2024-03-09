63bea2a63a45ab55bfc289a243761fa82415d88f
Andrea Ortez <andreaortez11@unitec.edu>
2024-03-09 16:01:08 -0600

    git commit -m "crud agentes" git push

--- backend/controllers/agent.controllers.js
+++ backend/controllers/agent.controllers.js
@@ -1,6 +1,6 @@
 import pool from "../db.js";
 //Primera prueba de insercion de momento sin prodecimiento almacenado hasta que el tigre lo enseñe
-export const insertAgent = async(req, res) => {
+/*export const insertAgent = async(req, res) => {
     try {
         const {
         identitynumber,
@@ -15,7 +15,26 @@ export const insertAgent = async(req, res) => {
         console.log(error)
         res.status(500).json({message: `An error ocurred: ${error.message}`});
     }
-}
+}*/
+
+export const insertAgent = async (req, res) => {
+    try {
+        const {
+            identitynumber,
+            name,
+            address,
+            phonenumber,
+            officephone
+        } = req.body;
+      await pool.query('CALL insertAgent($1, $2, $3, $4, $5)', [identitynumber, name, address, phonenumber, officephone]);
+      res.status(200).json({message: 'Success to add agent'});
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: `An error ocurred: ${error.message}`});
+    }finally {
+        await pool.end(); // Cierra la conexión al pool cuando hayas terminado
+      }
+  };
 
 export const getAgents = async(req, res) => {
     try {
@@ -26,3 +45,37 @@ export const getAgents = async(req, res) => {
         res.status(500).json({message: `An error ocurred: ${error.message}`});
     }
 }
+
+export const modifyAgent = async (req, res) => {
+    try {
+        const {
+            identitynumber,
+            name,
+            address,
+            phonenumber,
+            officephone
+        } = req.body;
+      await pool.query('CALL modifyAgent($1, $2, $3, $4, $5)', [identitynumber, name, address, phonenumber, officephone]);
+      res.status(200).json({message: 'Success to add agent'});
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: `An error ocurred: ${error.message}`});
+    }finally {
+        await pool.end();
+      }
+  };
+
+  export const deleteAgent = async (req, res) => {
+    try {
+        const {
+            identitynumber
+        } = req.body;
+      await pool.query('CALL deleteAgent($1)', [identitynumber]);
+      res.status(200).json({message: 'Success to add agent'});
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: `An error ocurred: ${error.message}`});
+    }finally {
+        await pool.end();
+      }
+  };
