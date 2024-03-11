56fb0a1b0f4df34f3abcf810c779778d8b685290
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2024-03-11 16:59:05 -0600

    Views in Routes

--- backend/routes/reports.routes.js
+++ backend/routes/reports.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getSalesxAgent } from "../controllers/reports.controllers.js";
+import { getSalesxAgent, citySales,buyerPurchases} from "../controllers/reports.controllers.js";
 
 const router =  Router();
 
 router.get('/getSalesxAgent', getSalesxAgent)
+router.get('/citySales', citySales)
+router.get('/buyerPurchases', buyerPurchases)
 
 export default router
