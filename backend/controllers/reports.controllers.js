eb9ba7cc084de1c8d0a33fa798a0a043de2861d5
HectorHernandez587 <eduhp587@gmail.com>
2024-03-12 10:54:45 -0600

    Vistas de vendedor, precio y caracteristicas

--- backend/controllers/reports.controllers.js
+++ backend/controllers/reports.controllers.js
@@ -33,3 +33,36 @@ export const citySales = async(req, res) => {
         res.status(500).json({ message: `An error ocurred: ${error.message}`})
     }
 }
+export const sellerSales = async(req, res) => {
+    try{
+        const resQuery = await pool.query(`SELECT * FROM ventas_por_vendedor`)
+        const fields = []
+        resQuery.fields.map(field => fields.push(field.name))
+        res.status(200).json({data: resQuery.rows, fields});
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ message: `An error ocurred: ${error.message}`})
+    }
+}
+export const property_priceSales = async(req, res) => {
+    try{
+        const resQuery = await pool.query(`SELECT * FROM ventas_por_precio`)
+        const fields = []
+        resQuery.fields.map(field => fields.push(field.name))
+        res.status(200).json({data: resQuery.rows, fields});
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ message: `An error ocurred: ${error.message}`})
+    }
+}
+export const featureSales = async(req, res) => {
+    try{
+        const resQuery = await pool.query(`SELECT * FROM ventas_por_caracteristicas`)
+        const fields = []
+        resQuery.fields.map(field => fields.push(field.name))
+        res.status(200).json({data: resQuery.rows, fields});
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ message: `An error ocurred: ${error.message}`})
+    }
+}
--- backend/routes/reports.routes.js
+++ backend/routes/reports.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { getSalesxAgent, citySales,buyerPurchases} from "../controllers/reports.controllers.js";
+import { getSalesxAgent, citySales,buyerPurchases, sellerSales, property_priceSales, featureSales} from "../controllers/reports.controllers.js";
 
 const router =  Router();
 
 router.get('/getSalesxAgent', getSalesxAgent)
 router.get('/citySales', citySales)
 router.get('/buyerPurchases', buyerPurchases)
+router.get('/sellerSales', sellerSales)
+router.get('/property_priceSales', property_priceSales)
+router.get('/featureSales', featureSales)
 
 export default router
