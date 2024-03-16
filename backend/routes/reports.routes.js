5be747d1559290f58594504a5e1a713d20cc8c31
Andrea Ortez <andreaortez11@unitec.edu>
2024-03-16 11:13:23 -0600

    reports and controllers for the last views

--- backend/routes/reports.routes.js
+++ backend/routes/reports.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getSalesxAgent, citySales,buyerPurchases, sellerSales, property_priceSales, featureSales} from "../controllers/reports.controllers.js";
+import { getSalesxAgent, citySales,buyerPurchases, sellerSales, property_priceSales, featureSales, bestsellingagent, agentsperformance} from "../controllers/reports.controllers.js";
 
 const router =  Router();
 
@@ -9,5 +9,7 @@ router.get('/buyerPurchases', buyerPurchases)
 router.get('/sellerSales', sellerSales)
 router.get('/property_priceSales', property_priceSales)
 router.get('/featureSales', featureSales)
+router.get('/bestsellingagent', bestsellingagent)
+router.get('/agentsperformance', agentsperformance)
 
 export default router
